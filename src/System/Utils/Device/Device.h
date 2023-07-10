d57b6a385849fdf6287d219619841f9eee347218
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-07-10 10:06:06 +0200

    device inherits from Stream class

--- src/System/Utils/Device/Device.h
+++ src/System/Utils/Device/Device.h
@@ -3,13 +3,22 @@
 
 #include <Arduino.h>
 
-class Device {
+class Device : public Stream {
    public:
     Device(){};
     ~Device(){};
+
+    int available();
+
     int seek(int pos);
+    int peek();
+    
+    void flush();
+
+    size_t write(const uint8_t data);
+    size_t write(const uint8_t* buffer, size_t size);
+
     int read();
-    int write(int str);
     int ioctl(int code, int var);
 };
 
