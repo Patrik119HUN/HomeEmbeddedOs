076d90d5e4b45759e9f497578813ec1a5fce0ad5
Patrik119HUN <tukacs.patrik77@gmail.com>
2023-07-23 12:33:02 +0200

    Device Manager with FS interface

--- src/System/DeviceManager/DeviceManager.h
+++ src/System/DeviceManager/DeviceManager.h
@@ -10,16 +10,16 @@
 #include "../Driver/drivers.h"
 #include "../Utils/Device/Device.h"
 #include "../sysmacros.h"
-
+#include "../FileSystem/IFile.h"
 class DeviceManager {
    private:
     inline static DeviceManager* instance{nullptr};
-    std::map<dev_t, Device*> devices;
+    std::map<dev_t, IFile*> devices;
     std::vector<int> deviceRecord;
     DeviceManager() { deviceRecord.assign(5, 0); }
     ~DeviceManager() = default;
 
-    int exits(Device* devPtr) {
+    int exits(IFile* devPtr) {
         for (auto i : devices) {
             if (i.second == devPtr) return 1;
         }
@@ -39,7 +39,7 @@ class DeviceManager {
     }
 
     enum devTypes { screen, rtcdev, relay, sd, sys };
-    dev_t addDev(devTypes type, Device* dev) {
+    dev_t addDev(devTypes type, IFile* dev) {
         dev_t a = deviceRecord.at(type);
         dev_t id = makedev(type, a);
         devices.insert(std::pair{id, dev});
@@ -47,7 +47,7 @@ class DeviceManager {
         return id;
     };
 
-    Device* getDev(dev_t id) { return devices.at(id); }
+    IFile* getDev(dev_t id) { return devices.at(id); }
 
     uint8_t rmdev(dev_t id) {
         devices.erase(id);
